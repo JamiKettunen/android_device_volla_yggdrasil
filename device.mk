4a842d3a96fb0569ecc3b596a1a4d2d17037edf9
Jami Kettunen <jami.kettunen@protonmail.com>
2023-04-13 19:23:11 +0300

    halium-overlay: Add NetworkManager config to fix hotspot passwords
    Password protected hotspot currently works but attempting to connect to
    it simply fails on UT 20.04.

--- device.mk
+++ device.mk
@@ -75,6 +75,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/halium-overlay/usr/share/upstart/sessions/mtp-server.conf:$(TARGET_COPY_OUT_SYSTEM)/halium/usr/share/upstart/sessions/mtp-server.conf \
     $(LOCAL_PATH)/halium-overlay/usr/share/usbinit/setupusb:$(TARGET_COPY_OUT_SYSTEM)/halium/usr/share/usbinit/setupusb \
     $(LOCAL_PATH)/halium-overlay/etc/deviceinfo/devices/halium.yaml:$(TARGET_COPY_OUT_SYSTEM)/halium/etc/deviceinfo/devices/halium.yaml \
+    $(LOCAL_PATH)/halium-overlay/etc/NetworkManager/conf.d/.halium-overlay-dir:$(TARGET_COPY_OUT_SYSTEM)/halium/etc/NetworkManager/conf.d/.halium-overlay-dir \
+    $(LOCAL_PATH)/halium-overlay/etc/NetworkManager/conf.d/yggdrasil-pmf-no-optional.conf:$(TARGET_COPY_OUT_SYSTEM)/halium/etc/NetworkManager/conf.d/yggdrasil-pmf-no-optional.conf \
     $(LOCAL_PATH)/halium-overlay/usr/share/repowerd/device-configs/config-default.xml:$(TARGET_COPY_OUT_SYSTEM)/halium/usr/share/repowerd/device-configs/config-default.xml
 
 PRODUCT_COPY_FILES += \
